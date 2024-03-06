0aaa5b6e2158d34303537c57e35a2d077bc00cb6
Mauricio Rojas <mrojasb2000@gmail.com>
2024-03-06 15:36:30 -0300

    feat: adding while operation with continue and break

--- While/main.zig
+++ While/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
+// Without a continue expression
 test "while" {
     var i: u8 = 2;
     while (i < 100) {
@@ -9,6 +10,7 @@ test "while" {
     try expect(i == 128);
 }
 
+// With a continue expression
 test "while with continue expression" {
     var sum: u8 = 0;
     var i: u8 = 1;
@@ -16,4 +18,26 @@ test "while with continue expression" {
         sum += i;
     }
     try expect(sum == 55);
+}
+
+// With a continue
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    try expect(sum == 4);
+}
+
+// With a break
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    try expect(sum == 1);
 }
