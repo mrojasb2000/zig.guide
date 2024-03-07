43d87094f34e3a5cf28bf1137cfbe4c0b3fac349
Mauricio Rojas <mrojasb2000@gmail.com>
2024-03-07 15:59:38 -0300

    refactor: adding const pointers

--- Pointers/main.zig
+++ Pointers/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
+// Pointer in Zig can't have 0 or null value
 // Referencing is done with &var
 // Dereferencing is done with var.*
 
@@ -12,4 +13,19 @@ test "pointers" {
     var x: u8 = 1;
     increment(&x);
     try expect(x == 2);
+}
+
+test "naughty pointer" {
+    // var x: u16 = 0;
+    // var y: *u8 = @ptrFromInt(x); //panic: cast causes pointer to be null
+    // _ = y;
+}
+
+// Zig also has const pointers, which can't be used to
+// modify the referenced data.
+test "const pointers" {
+    const x: u8 = 1;
+    var y = &x;
+    _ = y;
+    //y.* += 1; //error: cannot assign to constant
 }
