c85d195c8220bf757dd30f579110533b8261de5a
Mauricio Rojas <mrojasb2000@gmail.com>
2024-03-06 21:34:41 -0300

    feat: adding multi defer operator

--- Defer/main.zig
+++ Defer/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
+// Defer is used to execute a statement while existing the current block.
 test "defer" {
     var x: u16 = 5;
     {
@@ -8,4 +9,14 @@ test "defer" {
         try expect(x == 5);
     }
     try expect(x == 7);
+}
+
+// When there are multiple defers in a single block, they are executed in reverse order.
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    try expect(x == 4.5);
 }
