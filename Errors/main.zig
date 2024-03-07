62b27b3f5ffc79a0d1c9a46aa6e9c9677edba3d5
Mauricio Rojas <mrojasb2000@gmail.com>
2024-03-06 22:39:06 -0300

    refator: adding try/catch and errdefer operator

--- Errors/main.zig
+++ Errors/main.zig
@@ -13,3 +13,58 @@ test "coerce error from a subset to a superset" {
     const err: FileOpenError = AllocationError.OutOfMemory;
     try expect(err == FileOpenError.OutOfMemory);
 }
+
+// Union error set type and another type, union operator: !
+// Catch which is followed by an expression which is evaludated when the value
+// preceding it is error. The catch here is used to provide a fallback value.
+
+test "error union" {
+    const maybe_error: AllocationError!u16 = 10;
+    const no_error = maybe_error catch 0;
+
+    try expect(@TypeOf(no_error) == u16);
+    try expect(no_error == 10);
+}
+
+fn failingFunction() error{Oops}!void {
+    return error.Oops;
+}
+
+// Functions often return error unions.
+test "returning an error" {
+    failingFunction() catch |err| {
+        try expect(err == error.Oops);
+        return;
+    };
+}
+
+// try x is a shortcut for x catch |err| return err.
+fn failFn() error{Oops}!u32 {
+    try failingFunction();
+    return 32;
+}
+
+test "try" {
+    var v = failFn() catch |err| {
+        try expect(err == error.Oops);
+        return;
+    };
+    try expect(v == 12);
+}
+
+// errdefer works like defer, but only executing when the function is
+// returned from with error inside of the errdefer's block.
+var problems: u32 = 98;
+
+fn failFnCounter() error{Oops}!void {
+    errdefer problems += 1;
+    try failingFunction();
+}
+
+test "errdefer" {
+    failFnCounter() catch |err| {
+        try expect(err == error.Oops);
+        try expect(problems == 99);
+        return;
+    };
+}
